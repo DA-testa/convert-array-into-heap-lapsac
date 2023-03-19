8180bc66dcf53befc6314fe32aadc5451b40508f
Lauma Gailīte <lauma.gailite@edu.rtu.lv>
2023-03-19 15:35:48 +0200

    edit2

--- build_heap.py
+++ build_heap.py
@@ -1,35 +1,44 @@
 
 def build_heap(data):
-    n = len(data)
     swaps = []
-    for i in range(n):
+    size = len(data)
+
+    for i in range(size // 2, -1, -1):
         j = i
-        while j > 0:
-            parent = (j - 1)
-            if data [j] < data [parent]:
-                swaps.append((parent,j))
-                data[j], data[parent] = data[parent], data[j]
-                j = parent
-            else:
+        while True:
+            k = 2 * j + 1
+            if k >= size:
                 break
+            if k + 1 < size and data[k + 1] < data[k]:
+                k = k + 1
+            if data[j] <= data[k]:
+                break
+            swaps.append((j,k))
+            data[j], data[k] = data[k], data[k]
+            j = k
+
     return swaps
 
 def main():
-    n = int(input())
-    data = list(map(int, input().split()))
 
-    assert len(data) == n
-    assert all(0 <= data[i] <= 10**9 for i in range(n))
+    izvele = input()
+
+    if "I" in izvele:
+        m= int(input())
+        data = list(map(int, input().split()))
+    elif "F" in izvele:
+        fails = input()
+        if "a" not in fails:
+            dirrect = "./tests/" + fails
+            with open(dirrect, 'r', encoding = 'utf=8') as f:
+                m = int(f.readline())
+                data = list(map(int, f.readline().split()))
+
+    assert data is not None and len(data) == m
 
     swaps = build_heap(data)
 
-    for i in range(n):
-        left_child = 2*i / 1
-        right_child = 2*i / 2
-        if left_child < n:
-            assert data[i] <= data [left_child]
-        if right_child < n:
-            assert data[i] <= data [right_child]
+    assert len(swaps) <= 4*m
 
     print(len(swaps))
     for i, j in swaps:
