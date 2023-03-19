dd6dae02131a4340dfa0c16d3f5efc7dd2d8a835
Lauma Gailīte <123964250+lapsac@users.noreply.github.com>
2023-03-19 15:26:44 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,7 +1,6 @@
-
-def build_heap(dat):
+def build_heap(data):
     swaps = []
-    size = len(dat)
+    size = len(data)
 
     for i in range(size // 2, -1, -1):
         j = i
@@ -9,12 +8,12 @@ def build_heap(dat):
             k = 2*j+1
             if k >= size:
                 break
-            if k+1 < size and dat[k+1]<dat[k]:
+            if k+1 < size and data[k+1]<data[k]:
                 k=k+1
-            if dat[j]<=dat[k]:
+            if data[j]<=data[k]:
                 break
             swaps.append((j,k))
-            dat[j], dat[k] = dat[k], dat[k]
+            data[j], data[k] = data[k], data[k]
             j = k
 
     return swaps
@@ -25,18 +24,18 @@ def main():
 
     if "I" in izv:
         k= int(input())
-        dat = list(map(int, input().split()))
+        data = list(map(int, input().split()))
     elif "F" in izv:
         f = input()
         if not "k" in f:
             path = "./tests/"+ f
             with open(path,'r', encoding = 'utf=8') as f:
                 k = int(f.readline())
-                dat = list(map(int, f.readline().split()))
+                data = list(map(int, f.readline().split()))
 
-    assert dat is not None and len(dat) == k, "Invalid"
+    assert data is not None and len(data) == k, "Invalid"
 
-    swaps = build_heap(dat)
+    swaps = build_heap(data)
 
     assert len(swaps) <= 4*k
 
