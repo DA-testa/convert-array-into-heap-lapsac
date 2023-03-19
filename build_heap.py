542e590b08cd78ae465a4af21bca09e453c4cde4
Lauma Gailīte <123964250+lapsac@users.noreply.github.com>
2023-03-19 15:25:43 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,35 +1,44 @@
 
-def build_heap(data):
-    n = len(data)
+def build_heap(dat):
     swaps = []
-    for i in range(n):
+    size = len(dat)
+
+    for i in range(size // 2, -1, -1):
         j = i
-        while j > 0:
-            parent = (j - 1)
-            if data [j] < data [parent]:
-                swaps.append((parent,j))
-                data[j], data[parent] = data[parent], data[j]
-                j = parent
-            else:
+        while True:
+            k = 2*j+1
+            if k >= size:
                 break
+            if k+1 < size and dat[k+1]<dat[k]:
+                k=k+1
+            if dat[j]<=dat[k]:
+                break
+            swaps.append((j,k))
+            dat[j], dat[k] = dat[k], dat[k]
+            j = k
+
     return swaps
 
 def main():
-    n = int(input())
-    data = list(map(int, input().split()))
 
-    assert len(data) == n
-    assert all(0 <= data[i] <= 10**9 for i in range(n))
+    izv = input()
 
-    swaps = build_heap(data)
+    if "I" in izv:
+        k= int(input())
+        dat = list(map(int, input().split()))
+    elif "F" in izv:
+        f = input()
+        if not "k" in f:
+            path = "./tests/"+ f
+            with open(path,'r', encoding = 'utf=8') as f:
+                k = int(f.readline())
+                dat = list(map(int, f.readline().split()))
 
-    for i in range(n):
-        left_child = 2*i / 1
-        right_child = 2*i / 2
-        if left_child < n:
-            assert data[i] <= data [left_child]
-        if right_child < n:
-            assert data[i] <= data [right_child]
+    assert dat is not None and len(dat) == k, "Invalid"
+
+    swaps = build_heap(dat)
+
+    assert len(swaps) <= 4*k
 
     print(len(swaps))
     for i, j in swaps:
