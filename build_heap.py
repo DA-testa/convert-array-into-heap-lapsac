36ef493b76a859654bec781f934ca5bf8ee3969c
Lauma Gailīte <lauma.gailite@edu.rtu.lv>
2023-03-19 14:44:44 +0200

    edit1

--- build_heap.py
+++ build_heap.py
@@ -1,44 +1,36 @@
-# python3
-# import heapq
 
 def build_heap(data):
-    swaps = []
     n = len(data)
-    for i in range(n// 2 - 1, -1, -1):
-        heapify(data, n, i, swaps)
+    swaps = []
+    for i in range(n):
+        j = i
+        while j > 0:
+            parent = (j - 1)
+            if data [j] < data [parent]:
+                swaps.append((parent,j))
+                data[j], data[parent] = data[parent], data[j]
+                j = parent
+            else:
+                break
     return swaps
-    # TODO: Creat heap and heap sort
-
-    ExtractMax(n2)
-    # try to achieve  O(n) and not O(n2)
-
-
-    return swaps
-
 
 def main():
-    
-    # TODO : add input and corresponding checks
-    # add another input for I or F 
-    # first two tests are from keyboard, third test is from a file
-
-
-    # input from keyboard
     n = int(input())
     data = list(map(int, input().split()))
 
-    # checks if lenght of data is the same as the said lenght
     assert len(data) == n
+    assert all(0 <= data[i] <= 10**9 for i in range(n))
 
-    # calls function to assess the data 
-    # and give back all swaps
     swaps = build_heap(data)
 
-    # TODO: output how many swaps were made, 
-    # this number should be less than 4n (less than 4*len(data))
+    for i in range(n):
+        left_child = 2*i / 1
+        right_child = 2*i / 2
+        if left_child < n:
+            assert data[i] <= data [left_child]
+        if right_child < n:
+            assert data[i] <= data [right_child]
 
-
-    # output all swaps
     print(len(swaps))
     for i, j in swaps:
         print(i, j)
