5f1a9045eb0399cd132b6636bc67b4f2ac04a7dd
Lauma Gailīte <lauma.gailite@edu.rtu.lv>
2023-03-19 23:24:20 +0200

    edit3

--- build_heap.py
+++ build_heap.py
@@ -2,11 +2,10 @@
 def build_heap(data):
     swaps = []
     size = len(data)
-
     for i in range(size // 2, -1, -1):
         j = i
         while True:
-            k = 2 * j + 1
+            k = j * 2 + 1
             if k >= size:
                 break
             if k + 1 < size and data[k + 1] < data[k]:
@@ -19,27 +18,18 @@ def build_heap(data):
 
     return swaps
 
+
 def main():
-
     izvele = input()
-
     if "I" in izvele:
-        m= int(input())
+        n= int(input())
         data = list(map(int, input().split()))
     elif "F" in izvele:
-        fails = input()
-        if "a" not in fails:
-            dirrect = "./tests/" + fails
-            with open(dirrect, 'r', encoding = 'utf=8') as f:
+        f = input()
+        with open("tests/"+f,'r') as f:
                 m = int(f.readline())
                 data = list(map(int, f.readline().split()))
-
-    assert data is not None and len(data) == m
-
     swaps = build_heap(data)
-
-    assert len(swaps) <= 4*m
-
     print(len(swaps))
     for i, j in swaps:
         print(i, j)
